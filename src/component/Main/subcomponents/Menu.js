6a7189d0eff4aea7d3263dd13bf42710df36b98e
Tiffo <szuchinlin3@gmail.com>
2019-10-08 19:53:45 -0400

    started Menu section

--- src/component/Main/subcomponents/Menu.js
+++ src/component/Main/subcomponents/Menu.js
@@ -1,10 +1,23 @@
 import React from "react";
+import cake from "./images/cakes/1.PNG";
+import pastry from "./images/pastries/2.PNG";
+import chocolate from "./images/chocolates/1.PNG";
+
+
 
 function Menu() {
     return (
         <div id="Menu">
-            <h1 className="fontStyle">Menu </h1>
+            <p className="sectionTitle">MENU </p>
 
+        <div id="menuItemBox">
+            <span class="menuItems"><a href="#" ><img src={cake}></img></a></span>
+
+            <span class="menuItems"><a href="#" ><img src = {pastry}></img></a></span>  
+               
+            <span class="menuItems"><a href="#" ><img src = {chocolate}></img></a></span>
+         
+        </div>
   
         </div>
     )
--- src/component/Main/subcomponents/Slides.js
+++ src/component/Main/subcomponents/Slides.js
@@ -1,56 +1,78 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
-//images from gallery
-import chocobox from "./images/slides/chocobox.PNG";
-import pastries3 from "./images/slides/pastries3.PNG";
-import mb from "./images/slides/mb.PNG";
+//IMPORT IMAGES FROM GALLERY 
+//chocolates
+import choco1 from "./images/slides/slide1/1.PNG";
+import choco2 from "./images/slides/slide1/2.PNG";
+import choco3 from "./images/slides/slide1/3.PNG";
+import choco4 from "./images/slides/slide1/4.PNG";
+//pastries
+import pastrie1 from "./images/slides/slide2/1.PNG";
+import pastrie2 from "./images/slides/slide2/2.PNG";
+import pastrie3 from "./images/slides/slide2/3.PNG";
+import pastrie4 from "./images/slides/slide2/4.PNG";
+//cakes
+import cake1 from "./images/slides/slide3/1.PNG";
+import cake2 from "./images/slides/slide3/2.PNG";
+import cake3 from "./images/slides/slide3/3.PNG";
+import cake4 from "./images/slides/slide3/4.PNG";
+
 //css stylesheet
 import "./subcompStyle/styles.css";
 
 //CAROUSEL OF PRODUCTS
 function Slides() {
     return (
-        <div id="slideBody">
+        <section id="slideBody">
             <div id="slides1">
                 <Carousel>
                     <Carousel.Item>
-                    <img className="d-block w-100" src={chocobox} alt="First slide"/>
+                    <img className="d-block w-100" src={choco1} alt="First slide"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className="d-block w-100" src={pastries3} alt="Second slide"/>
+                        <img className="d-block w-100" src={choco2} alt="Second slide"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className="d-block w-100" src={mb} alt="Third slide"/>
+                        <img className="d-block w-100" src={choco3} alt="Third slide"/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={choco4} alt="Third slide"/>
                     </Carousel.Item>
                 </Carousel>
             </div>
             <div id="slides2">
                 <Carousel>
                     <Carousel.Item>
-                    <img className="d-block w-100" src={chocobox} alt="First slide"/>
+                    <img className="d-block w-100" src={pastrie1} alt="First slide"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className="d-block w-100" src={pastries3} alt="Second slide"/>
+                        <img className="d-block w-100" src={pastrie2} alt="Second slide"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className="d-block w-100" src={mb} alt="Third slide"/>
+                        <img className="d-block w-100" src={pastrie3} alt="Third slide"/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={pastrie4} alt="Third slide"/>
                     </Carousel.Item>
                 </Carousel>
             </div>
             <div id="slides3">
                 <Carousel>
                     <Carousel.Item>
-                    <img className="d-block w-100" src={chocobox} alt="First slide"/>
+                    <img className="d-block w-100" src={cake1} alt="First slide"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className="d-block w-100" src={pastries3} alt="Second slide"/>
+                        <img className="d-block w-100" src={cake2} alt="Second slide"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img className="d-block w-100" src={mb} alt="Third slide"/>
+                    <img className="d-block w-100" src={cake3} alt="First slide"/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={cake4} alt="Second slide"/>
                     </Carousel.Item>
                 </Carousel>
             </div>
-        </div>
+        </section>
     )
 }
 
