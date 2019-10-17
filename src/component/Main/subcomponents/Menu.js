b8e531c74e37cce9732f60feab7fd5d5d5b7bd7c
Tiffo <szuchinlin3@gmail.com>
2019-10-16 21:59:31 -0400

    menue overlay

--- src/component/Main/subcomponents/Menu.js
+++ src/component/Main/subcomponents/Menu.js
@@ -10,14 +10,35 @@ function Menu() {
         <div id="Menu">
             <p className="sectionTitle">MENU </p>
 
-        <div id="menuItemBox">
-            <span class="menuItems"><a href="#" ><img src={cake}></img></a></span>
+            <div id="menuItemContainer">
 
-            <span class="menuItems"><a href="#" ><img src = {pastry}></img></a></span>  
-               
-            <span class="menuItems"><a href="#" ><img src = {chocolate}></img></a></span>
-         
-        </div>
+                <div className=" overlay-image _9u "><a href="LINK_URL">
+                    <img className=" image _9v menuImg " src={cake} alt="Alt text" />
+                    <div className=" normal _9x ">
+                        <div className=" text _2 ">Image + text ORIGINAL</div>
+                    </div>
+                    <div className=" hover _9w ">
+                        <div className=" text _2 ">Background + text HOVER</div>
+                    </div>
+                    </a>
+                </div>
+
+
+                {/* <div className="menuItemsWrapper">
+                    <img className="menuImg" src={cake}></img>
+                    <div className="overlay">CAKE</div>
+                </div>
+
+                <div className="menuItemsWrapper">
+                    <img className="menuImg" src={pastry}></img>
+                    <a href="#"><div className="overlay"></div></a>
+                </div>
+
+                <div className="menuItemsWrapper">
+                    <img className="menuImg" src={chocolate}></img>
+                    <a href="#"><div className="overlay"></div></a>
+                </div>           */}
+            </div>
   
         </div>
     )
